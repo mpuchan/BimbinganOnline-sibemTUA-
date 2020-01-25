0f6f20a77ca033aa22dee5c20f36068be8fa0a57
unknown <iputumpu23@gmail.com>
2020-01-25 12:13:43 +0800

    add maps dashboard user

--- application/views/beranda/layout/footer.php
+++ application/views/beranda/layout/footer.php
@@ -6,67 +6,61 @@
           <h4><span>About Us</span></h4>
         </div>
         <div class="widget_content">
-          <p>Donec earum rerum hic tenetur ans sapiente delectus, ut aut reiciendise voluptat maiores alias consequaturs aut perferendis doloribus asperiores.</p>
+          <p>Politeknik Negeri Bali
+            Kampus Politeknik Negeri Bali,
+            Bukit Jimbaran, Kuta Selatan,
+            Badung - Bali</p>
           <ul class="contact-details-alt">
             <li><i class="fa fa-map-marker"></i>
-              <p><strong>Address</strong>: #2021 Lorem Ipsum</p>
+              <p><strong>Address</strong>: Jl. Raya Uluwatu No.45, Jimbaran, Kec. Kuta Selatan., Kabupaten Badung, Bali 80361</p>
             </li>
             <li><i class="fa fa-user"></i>
-              <p><strong>Phone</strong>:(+91) 9000-12345</p>
+              <p><strong>Phone</strong>: 0361-701981</p>
             </li>
             <li><i class="fa fa-envelope"></i>
-              <p><strong>Email</strong>: <a href="#">mail@example.com</a></p>
+              <p><strong>Email</strong>: <a href="mailto:poltek@pnb.ac.id">poltek@pnb.ac.id</a></p>
+              <p><a href="mailto:pkphi@pnb.ac.id">pkphi@pnb.ac.id (email internasional)</p>
             </li>
           </ul>
         </div>
       </div>
-      <div class="col-sm-6 col-md-4 col-lg-4">
+      <div class="col-sm-12 col-md-8 col-lg-8">
         <div class="widget_title">
-          <h4><span>Recent Posts</span></h4>
+          <h4><span>Maps</span></h4>
         </div>
         <div class="widget_content">
-          <ul class="links">
-            <li> <a href="#">Aenean commodo ligula eget dolor<span>November 07, 2014</span></a></li>
-            <li> <a href="#">Temporibus autem quibusdam <span>November 05, 2014</span></a></li>
-            <li> <a href="#">Debitis aut rerum saepe <span>November 03, 2014</span></a></li>
-            <li> <a href="#">Et voluptates repudiandae <span>November 02, 2014</span></a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-4">
-        <div class="widget_title">
-          <h4><span>Twitter</span></h4>
 
-        </div>
-        <div class="widget_content">
-          <ul class="tweet_list">
-            <li class="tweet_content item">
-              <p class="tweet_link"><a href="#">@jquery_rain </a> Lorem ipsum dolor et, consectetur adipiscing eli</p>
-              <span class="time">29 September 2014</span>
-            </li>
-            <li class="tweet_content item">
-              <p class="tweet_link"><a href="#">@jquery_rain </a> Lorem ipsum dolor et, consectetur adipiscing eli</p>
-              <span class="time">29 September 2014</span>
-            </li>
-            <li class="tweet_content item">
-              <p class="tweet_link"><a href="#">@jquery_rain </a> Lorem ipsum dolor et, consectetur adipiscing eli</p>
-              <span class="time">29 September 2014</span>
-            </li>
-          </ul>
-        </div>
-        <div class="widget_content">
-          <div class="tweet_go"></div>
+          <style>
+            .widget_content {
+              overflow: hidden;
+              padding-bottom: 28%;
+              position: relative;
+              height: 0;
+            }
+
+            .widget_content iframe {
+              height: 100%;
+              width: 100%;
+              left: 0;
+              top: 0;
+              position: absolute;
+            }
+          </style>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15771.445366675596!2d115.161653!3d-8.799095!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6c05042449b50f81!2sPoliteknik%20Negeri%20Bali!5e0!3m2!1sid!2sid!4v1579923022856!5m2!1sid!2sid" width="750" height="200" frameborder="0" style="border:0; border-radius: 10px;  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" allowfullscreen=""></iframe>
+
         </div>
       </div>
 
     </div>
+
+  </div>
   </div>
 </footer>
 <section class="footer_bottom">
   <div class="container">
     <div class="row">
       <div class="col-lg-6 ">
-        <p class="copyright"> &copy; Copyright <?php echo date('Y');  ?> Nopen rianto</p>
+        <p class="copyright"> &copy; Copyright <?php echo date('Y');  ?> POLITEKNIK NEGERI BALI</p>
       </div>
 
 
